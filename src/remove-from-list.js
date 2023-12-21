137f93dcf63794c5e2febd86d25bb61d7f15ab55
k98940 <pv1xxb@yandex.ru>
2023-12-21 10:55:53 +0600

    feat: removeKFromList. (Thu, Dec 21, 2023 10:55:53 AM)

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let root = l;
+
+  while (root.value === k) {
+    root = root.next;
+  }
+  let node = root;
+
+  while (node) {
+    let nextNode = node.next;
+    while (nextNode?.value === k) {
+      node.next = nextNode.next;
+      nextNode = nextNode.next;
+    }
+
+    node = nextNode;
+  }
+
+  return root
 }
 
 module.exports = {
